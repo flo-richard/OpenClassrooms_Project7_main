ca7d56959d250837002c45aa063564ea90014460
NicolasRanc <ranc.nicolas@gmail.com>
2022-10-01 15:03:49 +0200

    json et request pour api

--- API/app.py
+++ API/app.py
@@ -1,16 +1,22 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.get('/')
+# @app.get('/')
 
-def index():
-    return {'message': 'Hello World'}
+# def index():
+#     return {'message': 'Hello World'}
 
-@app.get('/{name}')
-def get_name(name: str):
-    return {'message': f'Hello, {name}'}
+@app.post('/{getPrediction}')
+async def get_name(info : Request):
+    req_info = await info.json()
+    # Ajouter les fonctions qui font le traitement ici
+    # Pour l'instant on renvoit l'info telle qu'on la reçoit
+    return {
+        "status": "SUCCESS",
+        "data": req_info
+        }
 
 if __name__ == '__main__':
     uvicorn.run(app, hist='127.0.0.1', port=8000)
